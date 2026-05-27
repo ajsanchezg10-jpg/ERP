95857c0775c3e8e8f2296ac23d2d838b3fa4b3ec
ajsanchezg10-jpg <aj.sanchezg10@gmail.com>
2026-05-27 12:13:44 -0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,20 @@
+{
+  "name": "ERP Constructora",
+  "short_name": "ERP",
+  "start_url": "./",
+  "display": "standalone",
+  "background_color": "#0A1628",
+  "theme_color": "#00C9A7",
+  "icons": [
+    {
+      "src": "assets/icon-192.png",
+      "sizes": "192x192",
+      "type": "image/png"
+    },
+    {
+      "src": "assets/icon-512.png",
+      "sizes": "512x512",
+      "type": "image/png"
+    }
+  ]
+}
